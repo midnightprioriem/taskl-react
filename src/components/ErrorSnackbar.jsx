60d28d0ea3181f61c8401f7ce5bf7bf7d3a2a08c
Zach Hannum <zacharyhannum@gmail.com>
2021-09-08 10:45:46 -0400

    Update error snackbar

--- src/components/ErrorSnackbar.jsx
+++ src/components/ErrorSnackbar.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Snackbar } from '@material-ui/core';
 import MuiAlert from '@material-ui/lab/Alert';
 
+import styles from './errorsnackbar.module.css';
+
 function Alert(props) {
     return <MuiAlert elevation={6} variant="filled" {...props} />;
 }
@@ -9,7 +11,7 @@ function Alert(props) {
 const ErrorSnackbar = (props) => {
     return (
         <Snackbar {...props} autoHideDuration={2000} >
-                    <Alert severity="error">
+                    <Alert severity="error" className={styles.snackbar}>
                         {props.children}
                     </Alert>
         </Snackbar>
